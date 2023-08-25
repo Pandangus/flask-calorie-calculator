ee4d5281d9327e9f85e9c53ffaf67843afd326a5
Angus <angushirst@outlook.com>
2023-08-25 17:13:21 +0100

    interface messages and ingredient list added

---
+++ src/retrieve_calories.py
@@ -0,0 +1,19 @@
+import requests
+from pprint import pprint
+
+
+def retrieve_calories(food_name, weight_in_grams):
+    try:
+        response = requests.get(
+            f"https://api.edamam.com/api/food-database/parser?app_id=ca747d07&app_key=722fabaee32b8118f7b1cb2e32b137cf&ingr=${food_name}"
+        )
+        json_response = response.json()
+        if len(json_response) == 4:
+            calories = json_response["hints"][0]["food"]["nutrients"]["ENERC_KCAL"]
+            return round(calories * (weight_in_grams / 100), 1)
+        else:
+            pprint(
+                "1 - No results found. Try checking spelling, or simplifying request."
+            )
+    except:
+        pprint("2 - Food name error. Try checking spelling.")
--- src/user_interface.py
+++ src/user_interface.py
@@ -1,26 +1,60 @@
-def menu():
-    while True:
-        
-        calorie_count = 0;
-        print("Total calories: ", calorie_count)
+from retrieve_calories import retrieve_calories
+from pprint import pprint
+import time
 
+
+def menu():
+    calorie_count = 0
+    ingredients = []
+    print("\nHello.")
+    time.sleep(0.75)
+    print("\nWelcome to the Calorie Counter.")
+    time.sleep(1)
+
+    while True:
+        print(f"\nTotal calories: {calorie_count}\n")
+        time.sleep(1)
         user_input = input(
-            "Please specify [e]nter calories, [d]elete calories, [l]ist total calories, [r]eset calories or e[x]it:\n"
-        )
+            "Please specify [e]nter calories, [d]elete calories, [l]ist total calories, [r]eset calories or e[x]it:\n\n"
+        ).lower()
 
         if user_input == "e":
-            print("You selected enter calories.")
+            time.sleep(0.25)
+            print("\nYou selected enter calories.")
+            time.sleep(0.25)
+            ingredient_user_input = input(
+                "\nPlease enter name of raw ingredient. (enter 'x' to cancel, and return to main menu)\n\n"
+            )
+            if ingredient_user_input != "x":
+                weight_user_input = input(
+                    "\nNow please enter weight in grams (g). (enter 'x' to cancel, and return to main menu)\n\n"
+                )
+                if weight_user_input != "x":
+                    calories = retrieve_calories(ingredient_user_input, int(weight_user_input))
+                    calorie_count += calories
+                    summary = f"\n{calories} kcal from {weight_user_input}g of {ingredient_user_input}"
+                    ingredients.append(summary)
+                    print(f"{summary} added")
 
         if user_input == "d":
             print("You selected delete calories.")
 
         if user_input == "r":
-            print("You selected reset calories.")
+            print("You selected reset calories.\nThis will reset total calories to 0 and erase the all current ingredients.")
+            reset_user_input = input("are you sure?\nenter:\n[y]es or [n]o\n")
+            if reset_user_input == "y":
+                calorie_count = 0
+                ingredients = []
 
         if user_input == "x":
-            user_input_2 = input("\nYou selected exit.\n-----------------\nare you sure?\nenter:\n[y]es or [n]o\n")
-            if user_input_2 == 'y':
+            user_input_2 = input(
+                "\nYou selected exit.\n-----------------\nare you sure?\nenter:\n[y]es or [n]o\n"
+            )
+            if user_input_2 == "y":
                 print("-------\nGoodbye\n-------")
                 break
+
+        # if user_input != 'e':
+
 
 menu()
