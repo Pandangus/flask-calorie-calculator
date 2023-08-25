4276df6965004a455e4c44ec387664a2d229a8d0
Angus <angushirst@outlook.com>
2023-08-25 17:18:25 +0100

    refactor reset prompt

--- src/user_interface.py
+++ src/user_interface.py
@@ -12,10 +12,10 @@ def menu():
     time.sleep(1)
 
     while True:
-        print(f"\nTotal calories: {calorie_count}\n")
+        print(f"\nTotal calories: {calorie_count}")
         time.sleep(1)
         user_input = input(
-            "Please specify [e]nter calories, [d]elete calories, [l]ist total calories, [r]eset calories or e[x]it:\n\n"
+            "\nPlease specify [e]nter calories, [d]elete calories, [l]ist total calories, [r]eset calories or e[x]it:\n\n"
         ).lower()
 
         if user_input == "e":
@@ -24,34 +24,39 @@ def menu():
             time.sleep(0.25)
             ingredient_user_input = input(
                 "\nPlease enter name of raw ingredient. (enter 'x' to cancel, and return to main menu)\n\n"
-            )
+            ).lower()
             if ingredient_user_input != "x":
                 weight_user_input = input(
                     "\nNow please enter weight in grams (g). (enter 'x' to cancel, and return to main menu)\n\n"
-                )
+                ).lower()
                 if weight_user_input != "x":
-                    calories = retrieve_calories(ingredient_user_input, int(weight_user_input))
+                    calories = retrieve_calories(
+                        ingredient_user_input, int(weight_user_input)
+                    )
                     calorie_count += calories
                     summary = f"\n{calories} kcal from {weight_user_input}g of {ingredient_user_input}"
                     ingredients.append(summary)
                     print(f"{summary} added")
+                else:
+                    print("\nreturn to main menu")
 
         if user_input == "d":
-            print("You selected delete calories.")
+            print("\nYou selected delete calories.")
 
         if user_input == "r":
-            print("You selected reset calories.\nThis will reset total calories to 0 and erase the all current ingredients.")
-            reset_user_input = input("are you sure?\nenter:\n[y]es or [n]o\n")
+            reset_user_input = input(
+                "\nYou selected reset calories.\nThis will reset total calories to 0 and erase xall current ingredients.\nare you sure?\nenter:\n[y]es or [n]o\n"
+            ).lower()
             if reset_user_input == "y":
                 calorie_count = 0
                 ingredients = []
 
         if user_input == "x":
             user_input_2 = input(
-                "\nYou selected exit.\n-----------------\nare you sure?\nenter:\n[y]es or [n]o\n"
-            )
+                "\nYou selected exit.\n-----------------\nare you sure?\nenter:\n[y]es or [n]o\n\n"
+            ).lower()
             if user_input_2 == "y":
-                print("-------\nGoodbye\n-------")
+                print("\n-------\nGoodbye\n-------")
                 break
 
         # if user_input != 'e':
