fc71e283a1086d180cc79cb48cb5a062b1764b35
Angus <angushirst@outlook.com>
2023-08-31 23:02:49 +0100

    menu header added

--- src/portion_calories.py
+++ src/portion_calories.py
@@ -4,16 +4,17 @@ from utility_functions.return_to_main_menu import return_to_main_menu
 
 
 def portion_calories(calories):
+    menu_header = "PORTION CALORIES\n----------------"
     time.sleep(0.25)
     portion_user_input = input(
-        "\nPORTION CALORIES\n----------------\nhow many portions? (enter 'x' to cancel, and return to main menu)\n\n-> "
+        f"\n{menu_header}\nhow many portions? (enter 'x' to cancel, and return to main menu)\n\n-> "
     ).lower()
     if portion_user_input != "x":
         processed_portion_input = round(
             float(re.search(r"[-+]?[0-9]*\.?[0-9]+", portion_user_input).group())
         )
         print(
-            f"\nPORTION CALORIES\n---------------------------\n{calories} total calories (kcal)\ndivided into {portion_user_input} portions\n---------------------------\n{round(calories / processed_portion_input)} calories per portion\n---------------------------"
+            f"\n{menu_header}\n{calories} total calories (kcal)\ndivided into {portion_user_input} portions\n---------------------------\n{round(calories / processed_portion_input)} calories per portion\n---------------------------"
         )
         return_to_main_menu()
     else:
