ea8be536462045ad73e0be73f1d8af355ec666de
Angus <angushirst@outlook.com>
2023-08-31 23:02:59 +0100

    menu header added

--- src/reset_calories.py
+++ src/reset_calories.py
@@ -3,16 +3,17 @@ from utility_functions.return_to_main_menu import return_to_main_menu
 
 
 def reset_calories():
+    menu_header = "RESET CALORIES"
     time.sleep(0.25)
     print("\nYou selected reset calories.")
     print(
-        "\nRESET CALORIES\n--------------------------------------------------------------------------------\nThis will reset total calories to 0 and erase all entries from ingredients list.\n--------------------------------------------------------------------------------\nThis cannot be undone!\n----------------------"
+        f"\n{menu_header}\n--------------------------------------------------------------------------------\nThis will reset total calories to 0 and erase all entries from ingredients list.\n--------------------------------------------------------------------------------\nThis cannot be undone!\n----------------------"
     )
     time.sleep(0.25)
     reset_user_input = input("\nare you sure?\nenter:\n[y]es or [n]o\n\n-> ").lower()
     if reset_user_input == "y":
         print(
-            "\nRESET CALORIES\n--------------\ncalories reset to 0. contents of ingredient list deleted\n"
+            f"\n{menu_header}\n--------------\ncalories reset to 0. contents of ingredient list deleted\n"
         )
         return ([], 0)
     else:
