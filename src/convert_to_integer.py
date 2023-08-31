22ef22ff75c712ab740fd44bdddcd25007e0c1d6
Angus <angushirst@outlook.com>
2023-08-31 19:15:05 +0100

    proper handling of none string type inputs added

--- src/convert_to_integer.py
+++ src/convert_to_integer.py
@@ -4,13 +4,15 @@ import re
 def convert_to_integer(value):
     try:
         if isinstance(value, str):
-            return round(float(re.search(r"[-+]?[0-9]*\.?[0-9]+", value).group()))
-        elif isinstance(value, (int, float)):
+            if any(chr.isdigit() for chr in value):
+                return round(float(re.search(r"[-+]?[0-9]*\.?[0-9]+", value).group()))
+            else:
+                print("\n----------------------------------------\ncould not extract number from user input\n----------------------------------------")
+                return None
+        if isinstance(value, (int, float)):
             return round(value)
-        else:
-            raise ValueError
     except ValueError:
         print(
-            "\n-------------------------------\nerror during integer conversion\n-------------------------------\n"
+            "\n-------------------------------\nerror during integer conversion\n-------------------------------"
         )
         return None
