7e8570a5f9431a1348275e6976e704fdc5af67f8
Angus <angushirst@outlook.com>
2023-08-31 19:29:38 +0100

    adjusting for readablity, no code changes otherwise

--- src/utility_functions/calories_to_add.py
+++ src/utility_functions/calories_to_add.py
@@ -2,5 +2,7 @@ def calories_to_add(calories_per_100g, weight_in_g):
     try:
         return round(calories_per_100g * (weight_in_g / 100))
     except ValueError:
-        print("\n------------------------------\nerror calculating new calories\n------------------------------\n")
+        print(
+            "\n------------------------------\nerror calculating new calories\n------------------------------\n"
+        )
         return None
--- src/utility_functions/convert_to_integer.py
+++ src/utility_functions/convert_to_integer.py
@@ -7,7 +7,9 @@ def convert_to_integer(value):
             if any(chr.isdigit() for chr in value):
                 return round(float(re.search(r"[-+]?[0-9]*\.?[0-9]+", value).group()))
             else:
-                print("\n----------------------------------------\ncould not extract number from user input\n----------------------------------------")
+                print(
+                    "\n----------------------------------------\ncould not extract number from user input\n----------------------------------------"
+                )
                 return None
         if isinstance(value, (int, float)):
             return round(value)
