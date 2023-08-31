d5df3bcafa4d6feff7a5c7050c2a77becd830b2f
Angus <angushirst@outlook.com>
2023-08-31 17:32:48 +0100

    error handling added

--- src/convert_to_integer.py
+++ src/convert_to_integer.py
@@ -2,7 +2,15 @@ import re
 
 
 def convert_to_integer(value):
-    if isinstance(value, str):
-        return round(float(re.search(r"[-+]?[0-9]*\.?[0-9]+", value).group()))
-    elif isinstance(value, (int, float)):
-        return round(value)
+    try:
+        if isinstance(value, str):
+            return round(float(re.search(r"[-+]?[0-9]*\.?[0-9]+", value).group()))
+        elif isinstance(value, (int, float)):
+            return round(value)
+        else:
+            raise ValueError
+    except ValueError:
+        print(
+            "\n-------------------------------\nerror during integer conversion\n-------------------------------\n"
+        )
+        return None
