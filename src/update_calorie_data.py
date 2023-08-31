9b3807ee11dfba6f54f5ab893ad4258b526bda74
Angus <angushirst@outlook.com>
2023-08-31 13:57:32 +0100

    update_calorie_data extracted into it's own function

---
+++ src/update_calorie_data.py
@@ -0,0 +1,23 @@
+from copy import deepcopy
+from convert_to_integer import convert_to_integer
+from calories_to_add import calories_to_add
+
+
+def update_calorie_data(
+    calories_per_100g,
+    weight_user_input,
+    ingredient_user_input,
+    ingredients_list,
+    total_calories,
+    menu_header,
+):
+    processed_weight_input = convert_to_integer(weight_user_input)
+    new_calories = calories_to_add(calories_per_100g, processed_weight_input)
+    total_calories += new_calories
+    summary = (
+        f"{new_calories} kcal from {weight_user_input}g of {ingredient_user_input}"
+    )
+    updated_ingredients = deepcopy(ingredients_list)
+    updated_ingredients.append(summary)
+    print(f"\n{menu_header}\nsuccess! {summary} added")
+    return updated_ingredients, total_calories
