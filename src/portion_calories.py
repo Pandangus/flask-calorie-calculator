9589e8c6f5df6028ddecad380b9f5195b35d51dc
Angus <angushirst@outlook.com>
2023-08-31 23:07:49 +0100

    error handling added

--- src/portion_calories.py
+++ src/portion_calories.py
@@ -4,18 +4,22 @@ from utility_functions.return_to_main_menu import return_to_main_menu
 
 
 def portion_calories(calories):
-    menu_header = "PORTION CALORIES\n----------------"
-    time.sleep(0.25)
-    portion_user_input = input(
-        f"\n{menu_header}\nhow many portions? (enter 'x' to cancel, and return to main menu)\n\n-> "
-    ).lower()
-    if portion_user_input != "x":
-        processed_portion_input = round(
-            float(re.search(r"[-+]?[0-9]*\.?[0-9]+", portion_user_input).group())
-        )
-        print(
-            f"\n{menu_header}\n{calories} total calories (kcal)\ndivided into {portion_user_input} portions\n---------------------------\n{round(calories / processed_portion_input)} calories per portion\n---------------------------"
-        )
-        return_to_main_menu()
-    else:
-        return_to_main_menu()
+    try:
+        menu_header = "PORTION CALORIES\n----------------"
+        time.sleep(0.25)
+        portion_user_input = input(
+            f"\n{menu_header}\nhow many portions? (enter 'x' to cancel, and return to main menu)\n\n-> "
+        ).lower()
+        if portion_user_input != "x":
+            processed_portion_input = round(
+                float(re.search(r"[-+]?[0-9]*\.?[0-9]+", portion_user_input).group())
+            )
+            print(
+                f"\n{menu_header}\n{calories} total calories (kcal)\ndivided into {portion_user_input} portions\n---------------------------\n{round(calories / processed_portion_input)} calories per portion\n---------------------------"
+            )
+            return_to_main_menu()
+        else:
+            return_to_main_menu()
+    except (TypeError, AttributeError):
+        print(f"\n{menu_header}\nan unexpected error occurred")
+        return return_to_main_menu()
