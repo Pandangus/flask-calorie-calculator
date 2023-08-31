abe8d5e87b81db7231df6b9abfaed8aedb0609e2
Angus <angushirst@outlook.com>
2023-08-31 23:18:51 +0100

    remove menu_header declaration

--- src/exit.py
+++ src/exit.py
@@ -3,11 +3,10 @@ from utility_functions.return_to_main_menu import return_to_main_menu
 
 
 def exit():
-    menu_header = "EXIT\n----"
     try:
         time.sleep(0.25)
         user_input_2 = input(
-            f"\nYou selected exit.\n\n{menu_header}\nThis will close the calorie counter.\n\n-----------------------\nData will not be saved.\n-----------------------\nare you sure?\nenter:\n[y]es or [n]o\n\n-> "
+            "\nYou selected exit.\n\nEXIT\n----\nThis will close the calorie counter.\n\n-----------------------\nData will not be saved.\n-----------------------\nare you sure?\nenter:\n[y]es or [n]o\n\n-> "
         ).lower()
         if user_input_2 == "y":
             print("\n-------\nGoodbye\n-------")
@@ -15,5 +14,5 @@ def exit():
         else:
             return return_to_main_menu()
     except Exception:
-        print(f"\n{menu_header}\nan unexpected error occurred")
+        print("\nEXIT\n----\nan unexpected error occurred")
         return return_to_main_menu()
