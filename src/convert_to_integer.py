af6e14c01e5abdb107619ce15295e659f9ec3148
Angus <angushirst@outlook.com>
2023-08-31 14:34:34 +0100

    refactor to enable function to accept float, integer or string values

--- src/convert_to_integer.py
+++ src/convert_to_integer.py
@@ -1,5 +1,8 @@
 import re
 
 
-def convert_to_integer(string):
-    return round(float(re.search(r"[-+]?[0-9]*\.?[0-9]+", string).group()))
+def convert_to_integer(value):
+    if isinstance(value, str):
+        return round(float(re.search(r"[-+]?[0-9]*\.?[0-9]+", value).group()))
+    elif isinstance(value, (int, float)):
+        return round(value)
