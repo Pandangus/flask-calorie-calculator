e860648b386114be51c4daa5e85c19ede624cb05
Angus <angushirst@outlook.com>
2023-08-31 23:15:34 +0100

    error handling added

--- src/exit.py
+++ src/exit.py
@@ -3,12 +3,17 @@ from utility_functions.return_to_main_menu import return_to_main_menu
 
 
 def exit():
-    time.sleep(0.25)
-    user_input_2 = input(
-        "\nYou selected exit.\n\nEXIT\n----\nThis will close the calorie counter.\n\n-----------------------\nData will not be saved.\n-----------------------\nare you sure?\nenter:\n[y]es or [n]o\n\n-> "
-    ).lower()
-    if user_input_2 == "y":
-        print("\n-------\nGoodbye\n-------")
-        return True
-    else:
+    menu_header = "EXIT\n----"
+    try:
+        time.sleep(0.25)
+        user_input_2 = input(
+            f"\nYou selected exit.\n\n{menu_header}\nThis will close the calorie counter.\n\n-----------------------\nData will not be saved.\n-----------------------\nare you sure?\nenter:\n[y]es or [n]o\n\n-> "
+        ).lower()
+        if user_input_2 == "y":
+            print("\n-------\nGoodbye\n-------")
+            return True
+        else:
+            return return_to_main_menu()
+    except Exception:
+        print(f"\n{menu_header}\nan unexpected error occurred")
         return return_to_main_menu()
