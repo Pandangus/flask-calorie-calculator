a986112244da4dbe5c213816f4b77a55b20babb2
Angus <angushirst@outlook.com>
2023-09-01 20:40:23 +0100

    clear terminal added

--- src/exit.py
+++ src/exit.py
@@ -1,3 +1,4 @@
+import os
 import time
 from utility_functions.return_to_main_menu import return_to_main_menu
 
@@ -5,10 +6,12 @@ from utility_functions.return_to_main_menu import return_to_main_menu
 def exit():
     try:
         time.sleep(0.25)
+        os.system('clear')
         user_input_2 = input(
             "\nYou selected exit.\n\nEXIT\n----\nThis will close the calorie counter.\n\n-----------------------\nData will not be saved.\n-----------------------\nare you sure?\nenter:\n[y]es or [n]o\n\n-> "
         ).lower()
         if user_input_2 == "y":
+            os.system('clear')
             print("\n-------\nGoodbye\n-------")
             return True
         else:
