be4b550fadc9e02691077346a1bd6b1bf2c11ae4
Angus <angushirst@outlook.com>
2023-09-01 20:40:35 +0100

    clear terminal added

--- src/list_total_calories.py
+++ src/list_total_calories.py
@@ -1,3 +1,4 @@
+import os
 import time
 from utility_functions.return_to_main_menu import return_to_main_menu
 
@@ -6,6 +7,7 @@ def list_total_calories(ingredientList, totalCalories):
     try:
         menu_header = "\nLIST TOTAL CALORIES\n------------------------"
         time.sleep(0.25)
+        os.system('clear')
         print(f"{menu_header}")
         for entry in ingredientList:
             time.sleep(0.25)
@@ -15,5 +17,5 @@ def list_total_calories(ingredientList, totalCalories):
             f"------------------------\n{totalCalories} kcal total\n------------------------"
         )
     except TypeError:
-        print(f"an unexpected error occurred")
+        print(f"{menu_header}\nan unexpected error occurred")
         return return_to_main_menu()
