e6926aaa2afc3de7c8aa337fb436044f31a54d7a
Angus <angushirst@outlook.com>
2023-09-03 23:18:08 +0100

    refactor adds option to manually enter calories if no results found

--- src/enter_calories.py
+++ src/enter_calories.py
@@ -3,47 +3,53 @@ import time
 import requests
 from utility_functions.return_to_main_menu import return_to_main_menu
 from utility_functions.update_calorie_data import update_calorie_data
+from manually_enter_calories import manually_enter_calories
 
 
 def enter_calories(ingredients_list, total_calories):
     try:
         menu_header = "ENTER CALORIES\n--------------"
         time.sleep(0.25)
-        os.system('clear')
+        os.system("clear")
         print("\nYou selected enter calories.")
         ingredient_user_input = input(
             f"\n{menu_header}\nPlease enter name of raw ingredient. (enter 'x' to cancel, and return to main menu)\n\n-> "
         ).lower()
         if ingredient_user_input != "x":
             time.sleep(0.25)
-            os.system('clear')
-            weight_user_input = input(
-                f"\n{menu_header}\nNow please enter weight in grams (g). (enter 'x' to cancel, and return to main menu)\n\n-> "
-            ).lower()
-            if weight_user_input != "x":
-                os.system('clear')
-                json_response = requests.get(
-                    f"https://api.edamam.com/api/food-database/parser?app_id=ca747d07&app_key=722fabaee32b8118f7b1cb2e32b137cf&ingr=${ingredient_user_input}"
-                ).json()
-                if len(json_response) == 4:
-                    calories_per_100g = json_response["hints"][0]["food"]["nutrients"][
-                        "ENERC_KCAL"
-                    ]
+            os.system("clear")
+            json_response = requests.get(
+                f"https://api.edamam.com/api/food-database/parser?app_id=ca747d07&app_key=722fabaee32b8118f7b1cb2e32b137cf&ingr=${ingredient_user_input}"
+            ).json()
+            if len(json_response) == 4:
+                calories_per_100g = json_response["hints"][0]["food"]["nutrients"][
+                    "ENERC_KCAL"
+                ]
+                weight_user_input = input(
+                    f"\n{menu_header}\nNow please enter weight in grams (g). (enter 'x' to cancel, and return to main menu)\n\n-> "
+                ).lower()
+                if weight_user_input != "x":
+                    os.system("clear")
                     return update_calorie_data(
                         calories_per_100g,
                         weight_user_input,
                         ingredient_user_input,
                         ingredients_list,
-                        total_calories
+                        total_calories,
                     )
                 else:
-                    time.sleep(0.25)
-                    print(
-                        f"\nno results found. try checking spelling, or simplifying request.\n\nreturning to main menu"
-                    )
-                    return
+                    return return_to_main_menu()
             else:
-                return return_to_main_menu()
+                time.sleep(0.25)
+                print("\nsearch request returned no results")
+                time.sleep(0.25)
+                user_choice = input(
+                    "\nwould you like to manually add calories?\n\nplease enter:\n\n[m]anually enter calories or [r]eturn to main menu\n\n-> "
+                ).lower()
+                if user_choice == "m":
+                    return manually_enter_calories(ingredients_list, total_calories)
+                else:
+                    return return_to_main_menu()
         else:
             return return_to_main_menu()
     except (ValueError, TypeError):
