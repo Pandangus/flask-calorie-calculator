1c48d97658dba5a92b4084d81a59445aa3bc7e77
Angus <angushirst@outlook.com>
2023-09-05 00:55:46 +0100

    save_calories functionality added

--- src/save_calories.py
+++ src/save_calories.py
@@ -1,2 +1,25 @@
+import re
+import os
+import time
+import pandas as pd
+
+
 def save_calories(ingredients):
-    return
+    try:
+        time.sleep(0.25)
+        os.system("clear")
+        user_input = input("\nSAVE CALORIES\n-------------\nEnter a name for this calorie list: (enter 'x' to return to main menu)\n\n-> ")
+        calories = []
+        weights = []
+        names = []
+        for row in ingredients:
+            calories.append(re.search(r"^[0-9]+", row).group())
+            weights.append(re.search(r"[0-9]+g", row).group())
+            names.append(re.search(r"(\w+)$", row).group())
+        df = pd.DataFrame(data={"calories": calories, "weight": weights, "name": names})
+        df.to_csv(f"/Users/angushirst/Northcoders_followup/calorie-calculator/saved_calorie_data/{user_input}.csv")
+    except TypeError:
+        print("ingredient list error\nreturning to main menu")
+        return
+
+
