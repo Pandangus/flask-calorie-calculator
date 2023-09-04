3e06e542239d608717e55dce74d78768efc90276
Angus <angushirst@outlook.com>
2023-09-04 23:38:15 +0100

    save_calories integrated into main function

--- src/main_user_interface.py
+++ src/main_user_interface.py
@@ -7,27 +7,28 @@ from delete_calories import delete_calories
 from enter_calories import enter_calories
 from manually_enter_calories import manually_enter_calories
 from portion_calories import portion_calories
+from save_calories import save_calories
 
 
 def menu():
     calorie_count = 0
     ingredients = []
-    os.system('clear')
+    os.system("clear")
     print("\nHello.")
     time.sleep(0.5)
-    os.system('clear')
+    os.system("clear")
     print("\nWelcome to the Calorie Counter.")
     time.sleep(0.25)
-    
+
     while True:
         time.sleep(0.25)
         print(f"\nTotal calories: {calorie_count}")
         user_input = input(
-            "\nMAIN MENU\n---------\nPlease specify [e]nter calories, [m]anually enter calories, [d]elete calories, [l]ist total calories, [p]ortion calories, [r]eset calories or e[x]it:\n\n-> "
+            "\nMAIN MENU\n---------\nSelect [e]nter calories, [m]anually enter calories, [d]elete calories, [l]ist total calories, [p]ortion calories, [s]ave calories, [r]eset calories or e[x]it:\n\n-> "
         ).lower()
 
         if user_input == "e":
-            os.system('clear')
+            os.system("clear")
             enter_calories_result = enter_calories(ingredients, calorie_count)
             if enter_calories_result:
                 ingredients, calorie_count = (
@@ -36,7 +37,7 @@ def menu():
                 )
 
         if user_input == "m":
-            os.system('clear')
+            os.system("clear")
             manually_enter_result = manually_enter_calories(ingredients, calorie_count)
             if manually_enter_result:
                 ingredients, calorie_count = (
@@ -45,7 +46,7 @@ def menu():
                 )
 
         if user_input == "d":
-            os.system('clear')
+            os.system("clear")
             delete_calories_result = delete_calories(ingredients, calorie_count)
             if delete_calories_result:
                 ingredients, calorie_count = (
@@ -54,15 +55,19 @@ def menu():
                 )
 
         if user_input == "l":
-            os.system('clear')
+            os.system("clear")
             list_total_calories(ingredients, calorie_count)
 
         if user_input == "p":
-            os.system('clear')
+            os.system("clear")
             portion_calories(calorie_count)
 
+        if user_input == "s":
+            os.system("clear")
+            save_calories(ingredients)
+
         if user_input == "r":
-            os.system('clear')
+            os.system("clear")
             reset_calories_result = reset_calories()
             if reset_calories_result:
                 ingredients, calorie_count = (
@@ -71,12 +76,12 @@ def menu():
                 )
 
         if user_input == "x":
-            os.system('clear')
+            os.system("clear")
             if exit() == True:
                 break
-        
+
         if user_input not in ["e", "m", "d", "l", "p", "r", "x"]:
-            os.system('clear')
+            os.system("clear")
 
 
 menu()
