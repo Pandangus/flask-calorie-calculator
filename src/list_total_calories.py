f9b2643a1c75f1fee751331e61e5b2bf274e3bd6
Angus <angushirst@outlook.com>
2023-09-05 17:12:33 +0100

    falsy return bug fixed

--- src/list_total_calories.py
+++ src/list_total_calories.py
@@ -3,19 +3,31 @@ import time
 from utility_functions.return_to_main_menu import return_to_main_menu
 
 
-def list_total_calories(ingredientList, totalCalories):
+def list_total_calories(ingredient_list, total_calories=None):
     try:
-        menu_header = "\nLIST TOTAL CALORIES\n------------------------"
+        MENU_HEADER = "\nLIST TOTAL CALORIES\n------------------------"
         time.sleep(0.25)
-        os.system('clear')
-        print(f"{menu_header}")
-        for entry in ingredientList:
+        if total_calories:
+            os.system('clear')
+            print(f"{MENU_HEADER}")
+        if len(ingredient_list) == 0:
+            print("ingredient list contains no entries\n")
             time.sleep(0.25)
-            print(entry)
-        time.sleep(0.25)
-        print(
-            f"------------------------\n{totalCalories} kcal total\n------------------------"
-        )
+            input("press enter to return to main menu\n\n-> ")
+            return return_to_main_menu()
+        else:    
+            for entry in ingredient_list:
+                time.sleep(0.25)
+                print(entry)
+            time.sleep(0.25)
+            if total_calories:
+                print(
+                    f"------------------------\n{total_calories} kcal total\n------------------------"
+                )
+            return True
     except TypeError:
-        print(f"{menu_header}\nan unexpected error occurred")
+        if total_calories:
+            print(f"{MENU_HEADER}\nan unexpected error occurred")
+        else:
+            print(f"\nan unexpected error occurred")
         return return_to_main_menu()
