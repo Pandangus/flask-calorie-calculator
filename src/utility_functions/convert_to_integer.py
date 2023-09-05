09c70b380f90e87de7bd1bfe3c8f7c5ba134e115
Angus <angushirst@outlook.com>
2023-09-05 17:40:14 +0100

    f string added and string parameter added

--- src/utility_functions/convert_to_integer.py
+++ src/utility_functions/convert_to_integer.py
@@ -1,16 +1,16 @@
 import re
+import time
 
 
-def convert_to_integer(value):
+def convert_to_integer(value, unit_of_measurement):
     try:
         if isinstance(value, str):
             if any(chr.isdigit() for chr in value):
                 return round(float(re.search(r"[-+]?[0-9]*\.?[0-9]+", value).group()))
             else:
-                print(
-                    "\n----------------------------------------\ncould not extract number from user input\n----------------------------------------"
-                )
-                return None
+                print(f"\nerror! could not parse integer from {unit_of_measurement} input. please enter either an integer (0, 1, 2) or float value (0.5, 1.5, 2.5)")
+                time.sleep(0.25)
+                return
         if isinstance(value, (int, float)):
             return round(value)
     except ValueError:
