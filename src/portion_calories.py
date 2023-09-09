53752abab7f0fc327836a343fa5490a64275b243
Angus <angushirst@outlook.com>
2023-09-09 16:26:52 +0100

    error handling added

--- src/portion_calories.py
+++ src/portion_calories.py
@@ -8,12 +8,13 @@ def portion_calories(calories):
     try:
         menu_header = "PORTION CALORIES\n----------------"
         time.sleep(0.25)
-        os.system('clear')
+        os.system("clear")
         portion_user_input = input(
             f"\n{menu_header}\nhow many portions? (enter 'x' to cancel, and return to main menu)\n\n-> "
-        ).lower()
+        ).strip().lower()
+
         if portion_user_input != "x":
-            os.system('clear')
+            os.system("clear")
             processed_portion_input = round(
                 float(re.search(r"[-+]?[0-9]*\.?[0-9]+", portion_user_input).group())
             )
@@ -21,8 +22,30 @@ def portion_calories(calories):
                 f"\n{menu_header}\n{calories} total calories (kcal)\ndivided into {portion_user_input} portions\n---------------------------\n{round(calories / processed_portion_input)} calories per portion\n---------------------------"
             )
             return
+
         else:
             return_to_main_menu()
-    except (TypeError, AttributeError):
-        print(f"\n{menu_header}\nan unexpected error occurred")
-        return return_to_main_menu()
+
+    except ImportError as e:
+        print(f"portion_calories - ImportError: {e}")
+
+    except AttributeError as e:
+        print(f"portion_calories - AttributeError: {e}")
+
+    except NameError as e:
+        print(f"portion_calories - NameError: {e}")
+
+    except TypeError as e:
+        print(f"portion_calories - TypeError: {e}")
+
+    except ValueError as e:
+        print(f"portion_calories - TypeError: {e}")
+
+    except re.error as e:
+        print(f"portion_calories - re.error (regex error): {e}")
+
+    except KeyboardInterrupt:
+        print("portion_calories - operation interrupted by the user.")
+
+    except Exception as e:
+        print(f"portion_calories - an unexpected error occurred: {e}")
