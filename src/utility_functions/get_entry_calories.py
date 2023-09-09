663f367c3a10e56dfbaf8bc8b669ac07c661ca1a
Angus <angushirst@outlook.com>
2023-09-10 00:38:04 +0100

    TypeError exception removed

--- src/utility_functions/get_entry_calories.py
+++ src/utility_functions/get_entry_calories.py
@@ -4,9 +4,6 @@ import re
 def get_entry_calories(entry_string):
     try:
         return round(float(re.search(r"^[0-9]+", entry_string).group()))
-    
-    except TypeError as e:
-        print(f"get_entry_calories - TypeError: {e}")
 
     except Exception as e:
         print(f"get_entry_calories - an unexpected error occurred: {e}")
