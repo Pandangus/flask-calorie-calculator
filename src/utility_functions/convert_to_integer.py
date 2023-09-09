7619013b91a1c24d4fa0d96a33eea03d89399fe8
Angus <angushirst@outlook.com>
2023-09-10 00:35:46 +0100

    add to error handling

--- src/utility_functions/convert_to_integer.py
+++ src/utility_functions/convert_to_integer.py
@@ -7,14 +7,19 @@ def convert_to_integer(value, unit_of_measurement):
         if isinstance(value, str):
             if any(chr.isdigit() for chr in value):
                 return round(float(re.search(r"[-+]?[0-9]*\.?[0-9]+", value).group()))
+
             else:
-                print(f"\nerror! could not parse integer from {unit_of_measurement} input. please enter either an integer (0, 1, 2) or float value (0.5, 1.5, 2.5)")
+                print(
+                    f"\nerror! could not parse integer from {unit_of_measurement} input. please enter either an integer (0, 1, 2) or float value (0.5, 1.5, 2.5)"
+                )
                 time.sleep(0.25)
                 return
+
         if isinstance(value, (int, float)):
             return round(value)
-    except ValueError:
-        print(
-            "\n-------------------------------\nerror during integer conversion\n-------------------------------"
-        )
-        return None
+
+    except TypeError as e:
+        print(f"convert_to_integer - TypeError: {e}")
+
+    except Exception as e:
+        print(f"convert_to_integer - an unexpected error occurred: {e}")
