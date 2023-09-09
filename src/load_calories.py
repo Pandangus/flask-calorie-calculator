500a045e3b57af07e4e0da2fba7b14cd46b84570
Angus <angushirst@outlook.com>
2023-09-09 15:36:25 +0100

    refactor for readability

--- src/load_calories.py
+++ src/load_calories.py
@@ -13,17 +13,22 @@ def load_calories(existing_entries, total_calories):
         time.sleep(0.25)
         print("\nLOAD CALORIES\n-------------")
         saved_file_count = 0
+
         for file in os.listdir(SAVED_FILES_DIR):
             if str(file)[-13:].lower() == "_calories.csv":
                 time.sleep(0.25)
+
                 if saved_file_count == 0:
                     print("\nsaved files:\n-------------")
+
                 saved_file_count += 1
                 print(">", re.search(r"^[a-z]+", file).group())
+
         if saved_file_count == 0:
             os.system("clear")
             print("\nno saved files found\n\nreturning to main menu")
             return None
+
         else:
             while True:
                 load_file_input = (
@@ -33,8 +38,10 @@ def load_calories(existing_entries, total_calories):
                     .strip()
                     .lower()
                 )
+
                 if load_file_input == "x":
                     return return_to_main_menu()
+
                 elif f"{load_file_input}_calories.csv" in os.listdir(SAVED_FILES_DIR):
                     os.system("clear")
                     loaded_entries = []
@@ -42,13 +49,16 @@ def load_calories(existing_entries, total_calories):
                         f"{SAVED_FILES_DIR}/{load_file_input}_calories.csv"
                     )
                     load_count = 0
+
                     while load_count < len(df):
                         loaded_entries.append(
                             f"{df.at[load_count, 'calories']} kcal from {df.at[load_count, 'weight']} of {df.at[load_count, 'name']}"
                         )
                         load_count += 1
+
                     print(f"success! {load_file_input} has been loaded")
                     loaded_calories = df["calories"].sum()
+
                     if len(existing_entries) > 0:
                         while True:
                             user_choice_input = (
@@ -58,6 +68,7 @@ def load_calories(existing_entries, total_calories):
                                 .strip()
                                 .lower()
                             )
+
                             if user_choice_input == "a":
                                 return add_current_to_loaded(
                                     loaded_calories,
@@ -66,32 +77,43 @@ def load_calories(existing_entries, total_calories):
                                     existing_entries,
                                     load_file_input,
                                 )
+
                             elif user_choice_input == "l":
                                 os.system("clear")
                                 print(
                                     f"success! current session has been replaced with the previously saved '{load_file_input}' session"
                                 )
                                 return loaded_entries, loaded_calories
+
                             elif user_choice_input == "x":
                                 return return_to_main_menu()
+
                             else:
                                 os.system("clear")
                                 print("user input error")
+
                     else:
                         return loaded_entries, loaded_calories
+
                 else:
                     print(
                         f"\n{load_file_input} could not be found in saved files\n\nplease enter [a]dd current calories or [l]oaded calories only (enter 'x' to return to main menu):\n\n-> "
                     )
+
     except ImportError as e:
         print(f"add_current_to_loaded - ImportError: {e}")
+
     except AttributeError as e:
         print(f"add_current_to_loaded - AttributeError: {e}")
+
     except NameError as e:
         print(f"add_current_to_loaded - NameError: {e}")
+
     except TypeError as e:
         print(f"add_current_to_loaded - TypeError: {e}")
+
     except KeyboardInterrupt:
         print("add_current_to_loaded - operation interrupted by the user.")
+
     except Exception as e:
         print(f"add_current_to_loaded - an unexpected error occurred: {e}")
