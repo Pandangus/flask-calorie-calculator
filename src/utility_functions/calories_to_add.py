1c678d14ac33e508f21e90a5877a1cd88c30677d
Angus <angushirst@outlook.com>
2023-09-09 22:01:10 +0100

    newline added to error messages

--- src/utility_functions/calories_to_add.py
+++ src/utility_functions/calories_to_add.py
@@ -1,8 +1,7 @@
 def calories_to_add(weight_in_g, calories_per_100g):
     try:
         return round(calories_per_100g * (weight_in_g / 100))
-    except ValueError:
+    except ValueError as e:
         print(
-            "\n------------------------------\nerror calculating new calories\n------------------------------\n"
+            f"\ncalories_to_add - ValueError: {e}\n"
         )
-        return None
