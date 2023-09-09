6a7fd237e7d9dc8109c35a1be837cc8a838b1280
Angus <angushirst@outlook.com>
2023-09-10 00:33:03 +0100

    error handling added

--- src/utility_functions/calories_to_add.py
+++ src/utility_functions/calories_to_add.py
@@ -1,7 +1,12 @@
 def calories_to_add(weight_in_g, calories_per_100g):
     try:
         return round(calories_per_100g * (weight_in_g / 100))
+
     except ValueError as e:
-        print(
-            f"\ncalories_to_add - ValueError: {e}\n"
-        )
+        print(f"calories_to_add - ValueError: {e}")
+
+    except TypeError as e:
+        print(f"add_current_to_loaded - TypeError: {e}")
+
+    except Exception as e:
+        print(f"add_current_to_loaded - an unexpected error occurred: {e}")
