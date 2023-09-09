65c75c232de27b5bb0560e57ba9fc9182831aaef
Angus <angushirst@outlook.com>
2023-09-10 00:52:32 +0100

    refactor and error handling added

--- src/utility_functions/convert_to_integer.py
+++ src/utility_functions/convert_to_integer.py
@@ -4,22 +4,19 @@ import time
 
 def convert_to_integer(value, unit_of_measurement):
     try:
-        if isinstance(value, str):
-            if any(chr.isdigit() for chr in value):
-                return round(float(re.search(r"[-+]?[0-9]*\.?[0-9]+", value).group()))
-
-            else:
-                print(
-                    f"\nerror! could not parse integer from {unit_of_measurement} input. please enter either an integer (0, 1, 2) or float value (0.5, 1.5, 2.5)"
-                )
-                time.sleep(0.25)
-                return
-
         if isinstance(value, (int, float)):
             return round(value)
 
-    except TypeError as e:
-        print(f"convert_to_integer - TypeError: {e}")
+        if isinstance(value, str):
+            re_match = re.search(r"[-+]?[0-9]*\.?[0-9]+", value)
+            if re_match:
+                return round(float(re_match.group()))
+
+        print(
+            f"\nerror! could not parse integer from {unit_of_measurement} input. please enter either an integer (0, 1, 2) or float value (0.5, 1.5, 2.5)"
+        )
+        time.sleep(0.25)
+        return
 
     except Exception as e:
         print(f"convert_to_integer - an unexpected error occurred: {e}")
