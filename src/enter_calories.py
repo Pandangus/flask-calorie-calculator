af42986980c10106468875c1cd772707ee7b592a
Angus <angushirst@outlook.com>
2023-09-09 22:48:51 +0100

    refactor for readability

--- src/enter_calories.py
+++ src/enter_calories.py
@@ -15,12 +15,14 @@ def enter_calories(ingredients_list, total_calories):
         ingredient_user_input = input(
             f"\n{menu_header}\nPlease enter name of raw ingredient. (enter 'x' to cancel, and return to main menu)\n\n-> "
         ).lower()
+
         if ingredient_user_input != "x":
             time.sleep(0.25)
             os.system("clear")
             json_response = requests.get(
                 f"https://api.edamam.com/api/food-database/parser?app_id=ca747d07&app_key=722fabaee32b8118f7b1cb2e32b137cf&ingr=${ingredient_user_input}"
             ).json()
+
             if len(json_response) == 4:
                 calories_per_100g = json_response["hints"][0]["food"]["nutrients"][
                     "ENERC_KCAL"
@@ -32,6 +34,7 @@ def enter_calories(ingredients_list, total_calories):
                     .strip()
                     .lower()
                 )
+
                 if weight_user_input != "x":
                     os.system("clear")
                     return update_calorie_data(
@@ -41,8 +44,10 @@ def enter_calories(ingredients_list, total_calories):
                         ingredients_list,
                         total_calories,
                     )
+
                 else:
                     return return_to_main_menu()
+
             else:
                 time.sleep(0.25)
                 print("\nsearch request returned no results")
@@ -50,13 +55,16 @@ def enter_calories(ingredients_list, total_calories):
                 user_choice = input(
                     "\nwould you like to manually add calories?\n\nplease enter:\n\n[m]anually enter calories or [r]eturn to main menu\n\n-> "
                 ).lower()
+
                 if user_choice == "m":
                     return manually_enter_calories(ingredients_list, total_calories)
+
                 else:
                     return return_to_main_menu()
+
         else:
             return return_to_main_menu()
-        
+
     except ImportError as e:
         print(f"\nmain_user_interface - ImportError: {e}")
 
