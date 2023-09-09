865774ac04e949b9a2f9698f7466aba1870cdc12
Angus <angushirst@outlook.com>
2023-09-10 00:36:36 +0100

    error handling added

--- src/utility_functions/get_entry_calories.py
+++ src/utility_functions/get_entry_calories.py
@@ -4,5 +4,9 @@ import re
 def get_entry_calories(entry_string):
     try:
         return round(float(re.search(r"^[0-9]+", entry_string).group()))
-    except TypeError:
-        print(f"could not parse calorie value from entry: {entry_string}")
+    
+    except TypeError as e:
+        print(f"get_entry_calories - TypeError: {e}")
+
+    except Exception as e:
+        print(f"get_entry_calories - an unexpected error occurred: {e}")
