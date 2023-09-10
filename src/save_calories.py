33ce141a7cf85c2fdc0a34c69e02aa86fd96334f
Angus <angushirst@outlook.com>
2023-09-10 23:20:57 +0100

    success message added

--- src/save_calories.py
+++ src/save_calories.py
@@ -30,6 +30,10 @@ def save_calories(ingredients):
             f"{SAVED_FILES_DIR}/{user_input}_calories.csv",
             index=False,
         )
+        os.system('clear')
+        print(f"success! '{user_input}' calorie entries were saved\n\nreturning to main menu")
+        return
+        
 
     except NameError as e:
         print(f"\nmain_user_interface - NameError: {e}")
