0b34c3d6532b7d8bd3fc12ace13527c85a3b123f
Angus <angushirst@outlook.com>
2023-09-10 01:10:29 +0100

    conditional block added

--- src/utility_functions/get_entry_calories.py
+++ src/utility_functions/get_entry_calories.py
@@ -4,7 +4,9 @@ import re
 def get_entry_calories(entry_string):
     try:
         re_match = re.search(r"^[0-9]+", entry_string)
-        return round(float(re_match.group()))
+        
+        if re_match:
+            return round(float(re_match.group()))
 
     except Exception as e:
         print(f"get_entry_calories - an unexpected error occurred: {e}")
