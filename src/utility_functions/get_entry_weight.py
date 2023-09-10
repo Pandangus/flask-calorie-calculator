f9efaa4da7157bd460de1ce4ab540946882c51d1
Angus <angushirst@outlook.com>
2023-09-10 01:03:52 +0100

    refactor and errorhandling added

--- src/utility_functions/get_entry_weight.py
+++ src/utility_functions/get_entry_weight.py
@@ -3,6 +3,16 @@ import re
 
 def get_entry_weight(entry_string):
     try:
-        return round(float(re.search(r"[0-9]+g", entry_string).group()[:-1]))
-    except TypeError:
-        print(f"could not parse weight value from entry: {entry_string}")
+        re_match = re.search(r"[0-9]+g", entry_string)
+
+        if re_match:
+            return round(float(re_match.group()[:-1]))
+
+    except IndexError as e:
+        print(f"get_entry_weight - TypeError: {e}")
+
+    except re.error as e:
+        print(f"get_entry_weight - re.error: {e}")
+
+    except Exception as e:
+        print(f"get_entry_weight - an unexpected error occurred: {e}")
