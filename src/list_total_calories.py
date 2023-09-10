96a9148a60eb27015bb67bc530dc54b2a49a1338
Angus <angushirst@outlook.com>
2023-09-10 01:47:35 +0100

    error handling expanded

--- src/list_total_calories.py
+++ src/list_total_calories.py
@@ -3,31 +3,41 @@ import time
 from utility_functions.return_to_main_menu import return_to_main_menu
 
 
-def list_total_calories(ingredient_list, total_calories=None):
+def list_total_calories(existing_ingredients, total_calories=None):
     try:
         MENU_HEADER = "\nLIST TOTAL CALORIES\n------------------------"
         time.sleep(0.25)
+
         if total_calories:
-            os.system('clear')
+            os.system("clear")
             print(f"{MENU_HEADER}")
-        if len(ingredient_list) == 0:
+
+        if len(existing_ingredients) == 0:
             print("ingredient list contains no entries\n")
             time.sleep(0.25)
             input("press enter to return to main menu\n\n-> ")
             return return_to_main_menu()
-        else:    
-            for entry in ingredient_list:
+
+        else:
+            for entry in existing_ingredients:
                 time.sleep(0.25)
                 print(entry)
             time.sleep(0.25)
+
             if total_calories:
                 print(
                     f"------------------------\n{total_calories} kcal total\n------------------------"
                 )
             return True
-    except TypeError:
-        if total_calories:
-            print(f"{MENU_HEADER}\nan unexpected error occurred")
-        else:
-            print(f"\nan unexpected error occurred")
-        return return_to_main_menu()
+        
+    except ValueError as e:
+        print(f"\nlist_total_calories - ValueError: {e}")
+
+    except TypeError as e:
+        print(f"\nlist_total_calories - TypeError: {e}")
+
+    except IndexError as e:
+        print(f"\nlist_total_calories - IndexError: {e}")
+
+    except Exception as e:
+        print(f"\nlist_total_calories - an unexpected error occurred: {e}")
