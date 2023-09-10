27fe6ec0c7ffac0739806d96f6b9d0863bfa1af8
Angus <angushirst@outlook.com>
2023-09-10 01:26:08 +0100

    ValueError handling added

--- src/delete_calories.py
+++ src/delete_calories.py
@@ -13,32 +13,38 @@ def delete_calories(ingredients_list, total_calories):
         print("\nYou selected delete calories.")
         time.sleep(0.25)
         print(f"\n{menu_header}")
+
         if not list_total_calories(ingredients_list):
             return None
-        delete_user_input = input(
-            f"\nPlease enter name of ingredient to remove\n\n-> "
-        )
+
+        delete_user_input = input(f"\nPlease enter name of ingredient to remove\n\n-> ")
         item_deleted = False
+
         for entry in ingredients_list:
             if delete_user_input.lower() in entry:
                 ingredients_list.remove(entry)
                 total_calories -= int(re.search(r"\d+", entry).group())
                 os.system("clear")
-                print(
-                    f"\nsuccess! {delete_user_input} removed from ingredient list"
-                )
+                print(f"\nsuccess! {delete_user_input} removed from ingredient list")
                 item_deleted = True
                 time.sleep(0.25)
                 print("\nreturning to main menu")
                 return (ingredients_list, total_calories)
+
         if not item_deleted:
-            os.system('clear')
+            os.system("clear")
             print(
                 f"\n{delete_user_input} returned no matches. Nothing was deleted from ingredients list."
             )
             time.sleep(0.25)
             print("\nreturning to main menu")
             return None
-    except TypeError:
-        print(f"\n{menu_header}\nan unexpected error occurred")
-        return return_to_main_menu()
+
+    except ValueError as e:
+        print(f"\ndelete_calories - ValueError: {e}")
+
+    except TypeError as e:
+        print(f"\ndelete_calories - TypeError: {e}")
+
+    except Exception as e:
+        print(f"\nudelete_calories - an unexpected error occurred: {e}")
