c49d2c6941edc964cc715eb35f75bb447fb1affa
Angus <angushirst@outlook.com>
2023-09-10 01:37:28 +0100

    while True loop added and error handling

--- src/exit.py
+++ src/exit.py
@@ -6,16 +6,31 @@ from utility_functions.return_to_main_menu import return_to_main_menu
 def exit():
     try:
         time.sleep(0.25)
-        os.system('clear')
-        user_input_2 = input(
-            "\nYou selected exit.\n\nEXIT\n----\nThis will close the calorie counter.\n\n-----------------------\nData will not be saved.\n-----------------------\nare you sure?\nenter:\n[y]es or [n]o\n\n-> "
-        ).lower()
-        if user_input_2 == "y":
-            os.system('clear')
-            print("\n-------\nGoodbye\n-------")
-            return True
-        else:
-            return return_to_main_menu()
-    except Exception:
-        print("\nEXIT\n----\nan unexpected error occurred")
-        return return_to_main_menu()
+        os.system("clear")
+        print("\nYou selected exit.\n\nEXIT\n----")
+
+        while True:
+            exit_user_input = (
+                input(
+                    "\ndo you want to exit calorie counter?\n\n-----------------------\nData will not be saved.\n-----------------------\n\nplease enter [y]es (enter 'x' to return to main menu)\n\n-> "
+                )
+                .strip()
+                .lower()
+            )
+
+            if exit_user_input == "x":
+                return return_to_main_menu()
+
+            if exit_user_input == "y":
+                os.system("clear")
+                print("\n-------\nGoodbye\n-------")
+                return True
+
+            os.system("clear")
+            print("invalid input")
+
+    except TypeError as e:
+        print(f"\nenter_calories - TypeError: {e}")
+
+    except Exception as e:
+        print(f"\nexit - an unexpected error occurred: {e}")
