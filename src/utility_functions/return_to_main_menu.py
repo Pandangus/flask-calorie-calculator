ea8b54545688ee41c402423c0ff23a5df3a205e0
Angus <angushirst@outlook.com>
2023-09-10 01:12:43 +0100

    general exception block added

--- src/utility_functions/return_to_main_menu.py
+++ src/utility_functions/return_to_main_menu.py
@@ -3,7 +3,11 @@ import time
 
 
 def return_to_main_menu():
-    time.sleep(0.25)
-    os.system("clear")
-    print("\nreturning to main menu")
-    return None
+    try:
+        time.sleep(0.25)
+        os.system("clear")
+        print("\nreturning to main menu")
+        return None
+
+    except Exception as e:
+        print(f"return_to_main_menu - an unexpected error occurred: {e}")
