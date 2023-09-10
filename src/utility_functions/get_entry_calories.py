c19f04a349ba22c36eb3e48719b0cb682cc72262
Angus <angushirst@outlook.com>
2023-09-10 01:09:44 +0100

    refactor to match other utilities procedures

--- src/utility_functions/get_entry_calories.py
+++ src/utility_functions/get_entry_calories.py
@@ -3,7 +3,8 @@ import re
 
 def get_entry_calories(entry_string):
     try:
-        return round(float(re.search(r"^[0-9]+", entry_string).group()))
+        re_match = re.search(r"^[0-9]+", entry_string)
+        return round(float(re_match.group()))
 
     except Exception as e:
         print(f"get_entry_calories - an unexpected error occurred: {e}")
