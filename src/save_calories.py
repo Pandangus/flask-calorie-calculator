71bcc2d08d39b682a65e73d77dcd42af5571a57d
Angus <angushirst@outlook.com>
2023-09-12 00:51:13 +0100

    menu header made consistent

--- src/save_calories.py
+++ src/save_calories.py
@@ -11,6 +11,7 @@ def save_calories(ingredients):
         SAVED_FILES_DIR = "saved_calorie_data"
         time.sleep(0.25)
         os.system("clear")
+        print("you selected save calories")
 
         if len(ingredients) == 0:
             print("no entries in current session\n\nreturning to main menu")
@@ -26,24 +27,25 @@ def save_calories(ingredients):
 
         if user_input == "x":
             return return_to_main_menu()
-        
+
         if f"{user_input}_calories.csv" in os.listdir(SAVED_FILES_DIR):
-            os.system('clear')
+            os.system("clear")
             while True:
-                existing_conflict_input = input(f"a file named '{user_input}' already exists\n\nare you sure you want to save? (saving now will overwrite the existing file)\n\nplease enter [s]ave and overwrite (enter 'x' to return to main menu):\n\n-> ")
+                existing_conflict_input = input(
+                    f"a file named '{user_input}' already exists\n\nare you sure you want to save? (saving now will overwrite the existing file)\n\nplease enter [s]ave and overwrite (enter 'x' to return to main menu):\n\n-> "
+                )
 
                 if existing_conflict_input == "x":
                     return return_to_main_menu()
-                
+
                 if existing_conflict_input == "s":
                     return save_to_csv(ingredients, SAVED_FILES_DIR, user_input)
-                
-                os.system('clear')
+
+                os.system("clear")
                 print("invalid input\n")
                 time.sleep(0.25)
 
         return save_to_csv(ingredients, SAVED_FILES_DIR, user_input)
-        
 
     except NameError as e:
         print(f"\nmain_user_interface - NameError: {e}")
