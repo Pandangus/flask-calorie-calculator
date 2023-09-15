d1d4a177f9713836cdad0e52c0383392931eca46
Angus <angushirst@outlook.com>
2023-09-15 21:52:09 +0100

    improve test name

--- tests/test_main_user_interface.py
+++ tests/test_main_user_interface.py
@@ -2,10 +2,9 @@ from unittest.mock import patch
 from src.main_user_interface import menu
 
 
-def test_ensures_internal_methods_are_each_called_once():
+def test_enter_calories_is_called_once():
     with patch("src.main_user_interface.enter_calories") as mock_enter_calories, patch(
-        "builtins.input", side_effect = ["e", "x", "x", "y"]
+        "builtins.input", side_effect = ["e", "x", "y"]
     ):
         menu()
-    
         assert mock_enter_calories.call_count == 1
