584183d5394a7c80cec7ea1b7d6af8ffd9a38711
Angus <angushirst@outlook.com>
2023-09-16 01:43:08 +0100

    modules/utility_functions/convert_to_integer.py

--- modules/utility_functions/convert_to_integer.py
+++ modules/utility_functions/convert_to_integer.py
@@ -12,11 +12,8 @@ def convert_to_integer(value, unit_of_measurement):
             if re_match:
                 return round(float(re_match.group()))
 
-        print(
-            f"\nerror! could not parse integer from {unit_of_measurement} input. please enter either an integer (0, 1, 2) or float value (0.5, 1.5, 2.5)"
+        raise Exception(f"error! could not parse integer from {unit_of_measurement} input. expected either an integer (0, 1, 2) or float value (0.5, 1.5, 2.5)"
         )
-        time.sleep(0.25)
-        return
 
     except Exception as e:
         print(f"convert_to_integer - an unexpected error occurred: {e}")
