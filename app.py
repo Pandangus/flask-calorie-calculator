f4c646e536974c8aa29d8ec0cb628d36f80c4ad7
Angus <angushirst@outlook.com>
2023-10-18 17:48:34 +0100

    Lists and Ingredients database model classes added

--- app.py
+++ app.py
@@ -19,10 +19,36 @@ class Users(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    lists = db.relationship("Lists", backref="user", lazy=True)
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+
+class Lists(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    list_name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    ingredients = db.relationship("Ingredient", backref="list", lazy=True)
+
+    def __init__(self, list_name, user_id):
+        self.name = list_name
+        self.user_id = user_id
+
+
+class Ingredients(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    ingredient_name = db.Column(db.String(100), nullable=False)
+    ingredient_weight = db.Column(db.Integer, nullable=False)
+    ingredient_calories = db.Column(db.Integer, nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey("list.id"), nullable=False)
+
+    def __init__(self, ingredient_name, ingredient_weight, ingredient_calories, list_id):
+        self.ingredient_name = ingredient_name
+        self.ingredient_weight = ingredient_weight
+        self.ingredient_calories = ingredient_calories
+        self.list_id = list_id
 
 
 with app.app_context():
