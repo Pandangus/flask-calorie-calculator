3f2f9810984989344b24da8087d0f3dc3bc06746
Angus <angushirst@outlook.com>
2023-10-23 18:04:15 +0100

    menu routes grouped and functional

--- routes/menus.py
+++ routes/menus.py
@@ -1,10 +1,31 @@
-from flask import render_template, flash, session
-from app import app
+from flask import Blueprint, render_template, flash, session
 
-@app.route("/")
+
+menu_bp = Blueprint('menu', __name__)
+
+
+@menu_bp.route("/")
 def index():
-    global calories
+
+    if "entries" not in session:
+        session["entries"] = []
+        
+    if "calories" not in session:
+        session["calories"] = 0
+
+    calories = session["calories"]
     if "username" in session:
         username = session["username"]
         flash(f"currently logged in as: {username}", "info")
     return render_template("navbar.html", calories=calories)
+
+
+@menu_bp.route("/my_account")
+def my_account():
+    if "username" in session:
+        username = session["username"]
+        flash(f"currently logged in as: {username}", "info")
+        return render_template("my_account.html")
+    else:
+        flash("not logged in", "info")
+        return render_template("login.html")
