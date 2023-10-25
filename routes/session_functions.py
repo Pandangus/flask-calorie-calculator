dc7ce29584eb3d599a8f6e9609e5eb31b0402583
Angus <angushirst@outlook.com>
2023-10-25 01:06:38 +0100

    session_functions.py blueprint added:

---
+++ routes/session_functions.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, request, render_template, redirect, url_for, session
+
+
+session_functions_bp = Blueprint("session_functions", __name__)
+
+
+@session_functions_bp.route("/reset_session", methods=["GET", "POST"])
+def reset_request():
+    if "entries" not in session:
+        session["entries"] = []
+    if "calories" not in session:
+        session["calories"] = 0
+
+    if request.method == "POST":
+        session["entries"] = []
+        session["calories"] = 0
+        return render_template("reset_confirmed.html")
+
+    return render_template("reset_request.html")
+
+
+@session_functions_bp.route("/list")
+def list():
+    if "entries" not in session:
+        session["entries"] = []
+    if "calories" not in session:
+        session["calories"] = 0
+
+    entries = session["entries"]
+    calories = session["calories"]
+    
+    return render_template("list.html", entries=entries, calories=calories)
