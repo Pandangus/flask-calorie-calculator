4c973ace2094a527da7b5c2ba64a44245d650d2b
Angus <angushirst@outlook.com>
2023-10-25 01:44:29 +0100

    delete account and change password moved to manage account blueprint

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ from modules.utility_functions.get_entry_weight import get_entry_weight
 from routes.delete_saved_list import delete_saved_list_bp
 from routes.session_functions import session_functions_bp
 from routes.portion_calories import portion_calories_bp
+from routes.authentication import authentication_bp
 from routes.delete_entries import delete_entries_bp
+from routes.manage_account import manage_account_bp
 from routes.add_entries import add_entries_bp
 from routes.menus import menu_bp
 from datetime import timedelta
 from models import Users, Lists, Ingredients, db, init_db
 from werkzeug.security import generate_password_hash, check_password_hash
-import re
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
@@ -29,116 +31,8 @@ app.register_blueprint(add_entries_bp)
 app.register_blueprint(delete_entries_bp)
 app.register_blueprint(portion_calories_bp)
 app.register_blueprint(session_functions_bp)
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        session.permanent = True
-        username = request.form["username"].strip().lower()
-        password = request.form["password"]
-        matched_user = Users.query.filter_by(username=username).first()
-        if matched_user:
-            if check_password_hash(matched_user.password, password):
-                session["username"] = username
-                flash(f"logged in as: {username}", "info")
-                return render_template("navbar.html")
-            else:
-                flash("invalid password")
-                return render_template("login.html")
-        else:
-            flash(f"no existing account for {username} was found")
-            return render_template("login.html")
-    else:
-        if "username" in session:
-            return redirect(url_for("logout.html"))
-        else:
-            return render_template("login.html")
-
-
-@app.route("/logout_request", methods=["GET", "POST"])
-def logout_request():
-    if "username" in session:
-        username = session["username"]
-        flash(f"currently logged in as: {username}", "info ")
-        flash("are you sure you want to log out?", "info")
-        return render_template("logout_request.html")
-    else:
-        flash("not logged in", "info")
-        return render_template("login.html")
-
-
-@app.route("/logout_confirm", methods=["GET", "POST"])
-def logout_confirm():
-    if "username" in session:
-        session.pop("username", None)
-        flash("you have been logged out", "info")
-        return render_template("navbar.html")
-    else:
-        flash("not logged in", "info")
-        return render_template("login.html")
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if "username" in session:
-        username = session["username"]
-        return redirect(url_for("logout_request"))
-    else:
-        if request.method == "POST":
-            username = request.form["username"].strip().lower()
-            password = request.form["password"]
-            re_enter_password = request.form["re-enter_password"]
-            usernames = Users.query.with_entities(Users.username).all()
-            usernames_list = [existing_username[0] for existing_username in usernames]
-            if username in usernames_list:
-                flash(f"an account already exists for: {username}")
-                return render_template("register.html")
-            if password == re_enter_password:
-                hashed_password = generate_password_hash(password, method="sha256")
-                new_user = Users(username=username, password=hashed_password)
-                db.session.add(new_user)
-                db.session.commit()
-                flash(f"{username} successfully registered")
-                return redirect(url_for("login"))
-            else:
-                flash("password entries did not match")
-                return render_template("register.html")
-        else:
-            return render_template("register.html")
-
-
-@app.route("/delete_account", methods=["GET"])
-def delete_account():
-    if "username" in session:
-        return render_template("delete_account.html")
-    else:
-        flash("not logged in", "info")
-        return render_template("login.html")
-
-
-@app.route("/delete_account_confirm", methods=["GET"])
-def delete_account_confirm():
-    if "username" in session:
-        return render_template("delete_account_confirm.html")
-    else:
-        flash("not logged in", "info")
-        return render_template("login.html")
-
-
-@app.route("/delete_account_execute", methods=["GET"])
-def delete_account_execute():
-    if "username" in session:
-        username = session["username"]
-        user_to_delete = Users.query.filter_by(username=username).first()
-        db.session.delete(user_to_delete)
-        db.session.commit()
-        session.pop("username", None)
-        flash("account successfully deleted")
-        return redirect(url_for("index"))
-    else:
-        flash("not logged in", "info")
-        return render_template("login.html")
+app.register_blueprint(authentication_bp)
+app.register_blueprint(manage_account_bp)
 
 
 @app.route("/save_entries_list", methods=["GET", "POST"])
@@ -252,38 +146,6 @@ def load_entries_list_complete():
     calories = loaded_calories
     entries = list
     return render_template("load_entries_list_complete.html", list_name=list_name)
-
-
-@app.route("/change_password", methods=["GET", "POST"])
-def change_password():
-    if "username" in session:
-        if request.method == "POST":
-            username = session["username"]
-            current_password = request.form["current_password"]
-            new_password = request.form["new_password"]
-            re_new_password = request.form["re_new_password"]
-            user_match = Users.query.filter_by(username=username).first()
-            if user_match:
-                if check_password_hash(user_match.password, current_password):
-                    if new_password == re_new_password:
-                        hashed_password = generate_password_hash(
-                            new_password, method="sha256"
-                        )
-                        user_match.password = hashed_password
-                        try:
-                            db.session.commit()
-                            return render_template("password_change_successful.html")
-                        except Exception as e:
-                            flash("error changing password - please try again later")
-
-                    else:
-                        flash("new passwords do not match")
-                else:
-                    flash("current password entered was invalid")
-        return render_template("change_password.html")
-    else:
-        flash("not logged in")
-        return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
