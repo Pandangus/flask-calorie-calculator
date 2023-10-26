a3a627c9ab056293f081d4f1a9118f41ed0b9170
Angus <angushirst@outlook.com>
2023-10-26 23:12:09 +0100

    move delete logic into delete confirmation route

--- routes/delete_saved_list.py
+++ routes/delete_saved_list.py
@@ -8,7 +8,6 @@ delete_saved_list_bp = Blueprint("delete_saved_list", __name__)
 @delete_saved_list_bp.route("/delete_saved_list", methods=["GET", "POST"])
 def delete_saved_list():
     username = session["username"]
-
     user = Users.query.filter_by(username=username).first()
     user_id = user.id
 
@@ -23,31 +22,35 @@ def delete_saved_list():
             return render_template("delete_saved_list.html", lists=user_lists_names)
 
         else:
-            try:
-                list = Lists.query.filter_by(
-                    list_name=list_name, user_id=user_id
-                ).first()
-                list_id = list.id
-                ingredients = Ingredients.query.filter_by(list_id=list_id)
-
-                for ingredient in ingredients:
-                    db.session.delete(ingredient)
-
-                db.session.delete(list)
-                db.session.commit()
-                return render_template(
-                    "delete_saved_list_confirm.html", list_name=list_name
-                )
-
-            except Exception as e:
-                flash("error deleting list - please try again later")
+            return render_template(
+                "delete_saved_list_confirm.html", list_name=list_name
+            )
 
     return render_template("delete_saved_list.html", lists=user_lists_names)
 
 
 @delete_saved_list_bp.route("/delete_saved_list_complete", methods=["GET"])
 def delete_saved_entries_list_complete():
-    list_name = request.args.get("list_name")
-    return render_template(
-        "delete_saved_entries_list_complete.html", list_name=list_name
-    )
+    try:
+        username = session["username"]
+        user = Users.query.filter_by(username=username).first()
+        user_id = user.id
+
+        list_name = request.args.get("list_name")
+        list = Lists.query.filter_by(list_name=list_name, user_id=user_id).first()
+        list_id = list.id
+
+        ingredients = Ingredients.query.filter_by(list_id=list_id)
+
+        for ingredient in ingredients:
+            db.session.delete(ingredient)
+
+        db.session.delete(list)
+        db.session.commit()
+
+        return render_template(
+            "delete_saved_list_complete.html", list_name=list_name
+        )
+
+    except Exception as e:
+        flash("error deleting list - please try again later")
