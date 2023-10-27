5dce9cd80d86029aaaf122eb1e9e8f8ed2c1e0a8
Angus <angushirst@outlook.com>
2023-10-27 18:16:05 +0100

    refactor for readability

--- routes/portion_calories.py
+++ routes/portion_calories.py
@@ -11,10 +11,10 @@ def portion_calories():
         form_portions = request.form.get("number_of_portions", type=int)
         calories_per_portion = round(calories / form_portions)
         return render_template(
-        "portion_result.html",
-        calories=calories,
-        form_portions=form_portions,
-        calories_per_portion=calories_per_portion,
-    )
+            "portion_result.html",
+            calories=calories,
+            form_portions=form_portions,
+            calories_per_portion=calories_per_portion,
+        )
 
     return render_template("portion_number.html")
