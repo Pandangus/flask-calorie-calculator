37ea18fd228761b3912a3155ef1a452c96dec2b7
Angus <angushirst@outlook.com>
2023-10-30 14:14:16 +0000

    docstring added

--- modules/utility_functions/update_calorie_data.py
+++ modules/utility_functions/update_calorie_data.py
@@ -1,8 +1,6 @@
-import re
-from copy import deepcopy
 from modules.utility_functions.convert_to_integer import convert_to_integer
 from modules.utility_functions.calories_to_add import calories_to_add
-from modules.utility_functions.replace_entry import replace_entry
+from copy import deepcopy
 
 
 def update_calorie_data(
@@ -12,13 +10,37 @@ def update_calorie_data(
     ingredients_list,
     total_calories,
 ):
+    """
+
+    Update calorie data based on user input.
+
+    This function takes user inputs, converts them to integers, calculates the calories
+    for the new entry, and updates the ingredients list and total calories accordingly.
+
+    Args:
+        calories_per_100g (str): Calories per 100g of the ingredient.
+        weight_user_input (str): Weight of the ingredient in grams.
+        ingredient_user_input (str): Name of the ingredient.
+        ingredients_list (list): List of existing ingredient entries.
+        total_calories (int): Total calories of all ingredients.
+
+    Returns:
+        tuple: A tuple containing the updated ingredients list and the new total calories,
+               or None if input conversion fails.
+
+    Raises:
+        TypeError: If input conversion to integers fails.
+        Exception: If an unexpected error occurs.
+
+    """
+
     try:
         new_entry_weight = convert_to_integer(weight_user_input, "weight")
         new_entry_calories_100g = convert_to_integer(calories_per_100g, "calories")
 
         if not new_entry_weight or not new_entry_calories_100g:
             return None
-        
+
         new_entry_calories = calories_to_add(new_entry_weight, new_entry_calories_100g)
         new_entry_summary = f"{new_entry_calories} kcal from {new_entry_weight}g of {ingredient_user_input}"
         total_calories += new_entry_calories
