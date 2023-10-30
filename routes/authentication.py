d88b73475b5862c72b95965b214f8bca7067d9bf
Angus <angushirst@outlook.com>
2023-10-30 13:59:19 +0000

    is_email_valid function integrated and try+except

--- routes/authentication.py
+++ routes/authentication.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
+from modules.utility_functions.validate_email import is_valid_email
 from models import Users, db
 
 
@@ -55,28 +56,59 @@ def logout():
 
 @authentication_bp.route("/register", methods=["GET", "POST"])
 def register():
+    """
+
+    Register a new user.
+
+    This route allows users to register by submitting a registration form. It checks for the presence
+    of the user's session and handles registration, email validation, and password confirmation.
+    Users are redirected to the login page upon successful registration.
+
+    Returns:
+        Flask response: Renders a template based on the registration outcome.
+        
+    """
+
     if "username" in session:
         username = session["username"]
         return redirect(url_for("logout"))
     else:
-        if request.method == "POST":
-            username = request.form["username"].strip().lower()
-            password = request.form["password"]
-            re_enter_password = request.form["re-enter_password"]
-            usernames = Users.query.with_entities(Users.username).all()
-            usernames_list = [existing_username[0] for existing_username in usernames]
-            if username in usernames_list:
-                flash(f"an account already exists for: {username}")
-                return render_template("register.html")
-            if password == re_enter_password:
-                hashed_password = generate_password_hash(password, method="sha256")
-                new_user = Users(username=username, password=hashed_password)
-                db.session.add(new_user)
-                db.session.commit()
-                flash(f"{username} successfully registered")
-                return render_template("login.html")
+        try:
+            if request.method == "POST":
+                username = request.form["username"].strip().lower()
+                email = request.form["email"].strip().lower()
+                password = request.form["password"]
+                re_enter_password = request.form["re-enter_password"]
+
+                usernames = Users.query.with_entities(Users.username).all()
+                usernames_list = [
+                    existing_username[0] for existing_username in usernames
+                ]
+
+                if username in usernames_list:
+                    flash(f"An account already exists for: {username}")
+                    return render_template("register.html")
+
+                if not is_valid_email(email):
+                    return render_template("register.html")
+
+                if password == re_enter_password:
+                    hashed_password = generate_password_hash(password, method="scrypt")
+                    new_user = Users(
+                        username=username, email=email, password=hashed_password
+                    )
+                    db.session.add(new_user)
+                    db.session.commit()
+                    flash(f"{username} successfully registered")
+                    return render_template("login.html")
+
+                else:
+                    flash("Password entries did not match")
+                    return render_template("register.html")
+
             else:
-                flash("password entries did not match")
                 return render_template("register.html")
-        else:
+            
+        except Exception as e:
+            flash(f"An unexpected error occurred: {e}")
             return render_template("register.html")
